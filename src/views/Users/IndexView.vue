9540f37a0f4a1c5830d9e10ccd90dbdf77553070
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-12-28 12:14:14 -0300

    first commit

---
+++ src/views/Users/IndexView.vue
@@ -0,0 +1,60 @@
+<template>
+  <a href=""  
+      class="text-white bg-blue-700 
+      hover:bg-blue-800 focus:ring-4 focus:ring-blue-300
+      font-medium rounded-lg text-sm px-5 py-2.5
+      mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 
+      focus:outline-none dark:focus:ring-blue-800">
+      +
+  </a>
+  <table 
+    class="mt-7 sm:rounded-lg w-5/6 text-sm text-left 
+          text-gray-500 dark:text-gray-400 shadow-2xl 
+          bg-gray-900">
+      <thead class="text-xs text-white uppercase dark:text-gray-400">
+          <tr>
+              <th scope="col" class="py-3 px-6">
+                Identificação
+              </th>
+              <th scope="col" class="py-3 px-6">
+                Nome
+              </th>
+              <th scope="col" class="py-3 px-6">
+                E-mail
+              </th>
+              <th scope="col" class="py-3 px-6">
+                Telefone fixo (opcional)
+              </th>
+              <th scope="col" class="py-3 px-6">
+                Telefone celular
+              </th>
+          </tr>
+      </thead>
+      <tbody>                
+        <tr class="hover:bg-gray-700">
+          <td class="py-4 px-6">
+            opa
+          </td>
+          <td class="py-4 px-6">
+            opa
+          </td>
+          <td class="py-4 px-6">
+            opa
+          </td>
+          <td class="py-4 px-6">
+            opa
+          </td>
+          <td class="py-4 px-6">
+            opa
+          </td>
+        </tr>
+      </tbody>
+  </table>
+</template>
+
+<script>
+
+export default {
+  name: 'IndexView',
+}
+</script>
